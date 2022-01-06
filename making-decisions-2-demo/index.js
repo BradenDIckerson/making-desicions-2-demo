52f17f4ed47f5833801f6e229979c02a75ecd796
Braden Dickerson <purpledragon6445@gmail.com>
2022-01-06 11:36:08 -0700

    progress

--- making-decisions-2-demo/index.js
+++ making-decisions-2-demo/index.js
@@ -1,1 +1,25 @@
 console.log ("Hello World")
+
+let backpack = []
+
+backpack.push("sword", "potion")
+backpack.push("Protien Bar")
+backpack.push("Protien Bar"
+backpack.push("sheild")
+backpack.push("candle")
+
+console.log(backpack)
+
+// let onBelt = backpack.splice(0, 1)
+let onBelt = backpack.shift()
+
+let furCoat = "fur coat"
+
+backpack.push(furCoat) 
+
+backpack.pop()
+console.log("backpack: backpack2, "Belt: ",onBelt item-count: ",itemCount") 
+
+let itemCount = backpack.length 
+
+let backpack2 = backpack.splice(3, 3)
